ec43a2bb1abb73ddaa48b68dce90fa520658bad6
hopeyen <hopey1118@gmail.com>
2023-08-15 11:32:02 -0500

    fix: timeout for message processing

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use dotenv::dotenv;
+use graphcast_sdk::{graphcast_agent::GraphcastAgent, WakuMessage};
 use listener_radio::{config::Config, operator::RadioOperator};
+use std::sync::mpsc;
 
 #[tokio::main]
 async fn main() {
@@ -7,12 +9,19 @@ async fn main() {
 
     // Parse basic configurations
     let radio_config = Config::args();
-
+    let (sender, receiver) = mpsc::channel::<WakuMessage>();
     // Initialization
-    let radio_operator = RadioOperator::new(radio_config).await;
+    let agent = GraphcastAgent::new(
+        radio_config.to_graphcast_agent_config().await.unwrap(),
+        sender,
+    )
+    .await
+    .expect("Initialize Graphcast agent");
+
+    let radio_operator = RadioOperator::new(radio_config, agent).await;
 
     // Start separate processes
-    radio_operator.prepare().await;
+    radio_operator.prepare(receiver).await;
 
     // Start radio operations
     radio_operator.run().await;
