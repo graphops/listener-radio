c8f043e67ab035558099b755a76c3815125164cb
Petko Pavlovski <pavlovskipetko@protonmail.com>
2024-02-01 20:00:58 +0000

    chore: update Graphcast SDK and fix conflicts (#27)

--- src/message_types.rs
+++ src/message_types.rs
@@ -2,6 +2,9 @@ use async_graphql::SimpleObject;
 use ethers_contract::EthAbiType;
 use ethers_core::types::transaction::eip712::Eip712;
 use ethers_derive_eip712::*;
+use graphcast_sdk::graphcast_agent::message_typing::{
+    GraphcastMessage, MessageError, RadioPayload,
+};
 use prost::Message;
 use serde::{Deserialize, Serialize};
 
@@ -19,8 +22,8 @@ pub struct PublicPoiMessage {
     pub content: String,
     //TODO: see if timestamp that comes with waku message can be used
     /// nonce cached to check against the next incoming message
-    #[prost(int64, tag = "3")]
-    pub nonce: i64,
+    #[prost(uint64, tag = "3")]
+    pub nonce: u64,
     /// blockchain relevant to the message
     #[prost(string, tag = "4")]
     pub network: String,
@@ -35,10 +38,31 @@ pub struct PublicPoiMessage {
     pub graph_account: String,
 }
 
+impl RadioPayload for PublicPoiMessage {
+    /// Check duplicated fields: payload message has duplicated fields with GraphcastMessage, the values must be the same
+    fn valid_outer(&self, outer: &GraphcastMessage<Self>) -> Result<&Self, MessageError> {
+        if self.nonce == outer.nonce
+            && self.graph_account == outer.graph_account
+            && self.identifier == outer.identifier
+        {
+            Ok(self)
+        } else {
+            Err(MessageError::InvalidFields(anyhow::anyhow!(
+                "Radio message wrapped by inconsistent GraphcastMessage: {:#?} <- {:#?}\nnonce check: {:#?}\naccount check: {:#?}\nidentifier check: {:#?}",
+                &self,
+                &outer,
+                self.nonce == outer.nonce,
+                self.graph_account == outer.graph_account,
+                self.identifier == outer.identifier,
+            )))
+        }
+    }
+}
+
 /// Make a test radio type
 #[derive(Eip712, EthAbiType, Clone, Message, Serialize, Deserialize, SimpleObject)]
 #[eip712(
-    name = "Graphcast Ping-Pong Radio",
+    name = "PingPongMessage",
     version = "0",
     chain_id = 1,
     verifying_contract = "0xc944e90c64b2c07662a292be6244bdf05cda44a7"
@@ -50,6 +74,20 @@ pub struct SimpleMessage {
     pub content: String,
 }
 
+impl RadioPayload for SimpleMessage {
+    fn valid_outer(&self, outer: &GraphcastMessage<Self>) -> Result<&Self, MessageError> {
+        if self.identifier == outer.identifier {
+            Ok(self)
+        } else {
+            Err(MessageError::InvalidFields(anyhow::anyhow!(
+                "Radio message wrapped by inconsistent GraphcastMessage: {:#?} <- {:#?}",
+                &self,
+                &outer,
+            )))
+        }
+    }
+}
+
 #[derive(Eip712, EthAbiType, Clone, Message, Serialize, Deserialize, PartialEq, SimpleObject)]
 #[eip712(
     name = "UpgradeIntentMessage",
@@ -58,16 +96,34 @@ pub struct SimpleMessage {
     verifying_contract = "0xc944e90c64b2c07662a292be6244bdf05cda44a7"
 )]
 pub struct UpgradeIntentMessage {
-    /// subgraph id shared by both versions of the subgraph deployment
+    /// current subgraph deployment hash
     #[prost(string, tag = "1")]
+    pub deployment: String,
+    /// subgraph id shared by both versions of the subgraph deployment
+    #[prost(string, tag = "2")]
     pub subgraph_id: String,
     // new version of the subgraph has a new deployment hash
-    #[prost(string, tag = "2")]
+    #[prost(string, tag = "3")]
     pub new_hash: String,
     /// nonce cached to check against the next incoming message
-    #[prost(int64, tag = "3")]
-    pub nonce: i64,
+    #[prost(uint64, tag = "4")]
+    pub nonce: u64,
     /// Graph account sender - expect the sender to be subgraph owner
-    #[prost(string, tag = "4")]
+    #[prost(string, tag = "5")]
     pub graph_account: String,
+}
+
+impl RadioPayload for UpgradeIntentMessage {
+    /// Check duplicated fields: payload message has duplicated fields with GraphcastMessage, the values must be the same
+    fn valid_outer(&self, outer: &GraphcastMessage<Self>) -> Result<&Self, MessageError> {
+        if self.nonce == outer.nonce && self.graph_account == outer.graph_account {
+            Ok(self)
+        } else {
+            Err(MessageError::InvalidFields(anyhow::anyhow!(
+                "Radio message wrapped by inconsistent GraphcastMessage: {:#?} <- {:#?}",
+                &self,
+                &outer,
+            )))
+        }
+    }
 }
